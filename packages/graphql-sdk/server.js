268f907367ee5ae0ab48d9cfebe643563c3b7060
Nick Vincent <emailnjv@gmail.com>
2019-04-23 18:20:37 -0400

    Possible zeit config

--- packages/graphql-sdk/server.js
+++ packages/graphql-sdk/server.js
@@ -1,19 +1,38 @@
-require('babel-polyfill')
-const express = require('express')
-const graphqlHTTP = require('express-graphql')
-const { schema } = require('./main')
-const livepeer = require('./main/mock-sdk.js').default
-const app = express()
+require( 'babel-polyfill' )
+const express = require( 'express' )
+// const graphqlHTTP = require('express-graphql')
+const { schema } = require( './main' )
+const { ApolloServer, gql } = require( 'apollo-server-express' );
+const { LivepeerSDK } = require( '@livepeer/sdk' );
+const { createServer } = require( 'http' );
+const app = express();
 
-app.use(
-  '/',
-  graphqlHTTP(req => ({
+const serve = async () => {
+  const livepeer = await LivepeerSDK();
+  // Your Livepeer SDK instance is now ready to use
+  // app.use(
+  //   '/',
+  //   graphqlHTTP( req => ( {
+  //     schema,
+  //     context: { livepeer },
+  //     graphiql: true,
+  //   } ) ),
+  // )
+  const server = new ApolloServer( {
     schema,
     context: { livepeer },
-    graphiql: true,
-  })),
-)
+  } )
+  server.applyMiddleware( { app, path: '/graphql' } );
+  const httpServer = createServer( app );
+  server.installSubscriptionHandlers( httpServer );
+  httpServer.listen( () => {
+    // httpServer.listen( { port: 8000 }, () => {
+    console.log( 'Apollo Server on http://localhost:8000/graphql' );
+  } );
+  // app.listen( 4000, () => {
+  //   console.log( 'GraphiQL server running at http://localhost:4000' )
+  // } )
+};
 
-app.listen(4000, () => {
-  console.log('GraphiQL server running at http://localhost:4000')
-})
+module.exports = serve()
+// serve()
