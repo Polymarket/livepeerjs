9ebd76650aee51f1de740c99719e8a243667c108
Nick Vincent <emailnjv@gmail.com>
2019-05-05 20:36:36 -0400

    Updated description to server file in the graphql-server

--- packages/graphql-sdk/server.js
+++ packages/graphql-sdk/server.js
@@ -1,38 +1,40 @@
-require( 'babel-polyfill' )
-const express = require( 'express' )
+require('babel-polyfill')
+const express = require('express')
 // const graphqlHTTP = require('express-graphql')
-const { schema } = require( './main' )
-const { ApolloServer, gql } = require( 'apollo-server-express' );
-const { LivepeerSDK } = require( '@livepeer/sdk' );
-const { createServer } = require( 'http' );
-const app = express();
+const { schema } = require('./main')
+const { ApolloServer, gql } = require('apollo-server-express')
+const { LivepeerSDK } = require('@livepeer/sdk')
+const { createServer } = require('http')
+
+// Init express
+const app = express()
 
 const serve = async () => {
-  const livepeer = await LivepeerSDK();
-  // Your Livepeer SDK instance is now ready to use
-  // app.use(
-  //   '/',
-  //   graphqlHTTP( req => ( {
-  //     schema,
-  //     context: { livepeer },
-  //     graphiql: true,
-  //   } ) ),
-  // )
-  const server = new ApolloServer( {
+  // Get the livepeer sdk context
+  const livepeer = await LivepeerSDK()
+
+  // Create apollo server
+  const server = new ApolloServer({
     schema,
     context: { livepeer },
-  } )
-  server.applyMiddleware( { app, path: '/graphql' } );
-  const httpServer = createServer( app );
-  server.installSubscriptionHandlers( httpServer );
-  httpServer.listen( () => {
-    // httpServer.listen( { port: 8000 }, () => {
-    console.log( 'Apollo Server on http://localhost:8000/graphql' );
-  } );
-  // app.listen( 4000, () => {
-  //   console.log( 'GraphiQL server running at http://localhost:4000' )
-  // } )
-};
+  })
 
-module.exports = serve()
-// serve()
+  // Apply the apollo middlewre to express
+  server.applyMiddleware({ app, path: '/graphql' })
+
+  // Create the server
+  const httpServer = createServer(app)
+  server.installSubscriptionHandlers(httpServer)
+
+  // Init the port to the server
+  // httpServer.listen( () => {
+  httpServer.listen({ port: 8000 }, () => {
+    console.log('Apollo Server on http://localhost:8000/graphql')
+  })
+}
+
+// Expose for Zeit
+// module.exports = serve()
+
+// Serve locally
+serve()
